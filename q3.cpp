16d4560a3baf5e47a042e8fdbf00342e70db6a88
FarihaJahanRainy <fariha.rainy23@gmailcom>
2021-07-12 13:37:53 +0600

    first version upload

---
+++ q3.cpp
@@ -0,0 +1,157 @@
+/*#include<iostream>
+#include<string>
+//#define PI 3.14
+using namespace std;
+class Register{
+private:
+    string register_number = "";
+    float registance_value = 0.0f;
+
+
+public:
+    Register()
+   {
+
+   }
+};
+int main()
+{
+    cout<<"hello world!\n";
+    cout<<"test class demo \n";
+    int PI=3.14;
+    cout<< PI;
+    Register r;
+    return 0;
+}*/
+
+/*#include<iostream>
+using namespace std;
+void swapValues(int *x,int *y)
+{
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+int main()
+{
+    int a =2,b=3;
+    swapValues(&a,&b);
+    cout<<a<<" "<<b;
+    int i=1;
+    i+=2;
+    cout<<i;
+    int x
+}
+*/
+/*#include<iostream>
+using namespace std;
+int main()
+{
+
+    int choice = 1;
+    switch(choice)
+    {
+    case 1:
+        {
+            cout <<"in case 1"<<endl;
+        }
+    case 2:
+        {
+             cout <<"in case 2"<<endl;
+             break;
+        }
+        default:
+        {
+              cout <<"in default"<<endl;
+        }
+    }
+}*/
+
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    for(n=5;n>0;n--)
+{
+    cout<<n;
+    if(n==3)
+        break;
+
+}
+return 0;
+}
+*/
+
+/*#include<iostream>
+using namespace std;
+int x = 3;
+int getX(){return x;
+}
+int main()
+
+{
+    int z=getX();
+    cout<<x<<" "<<getX()+2<<endl;
+   int sum = 23/16;
+   cout<<sum<<endl;
+   string a ="Hello";
+   cout<<a;
+   string b;
+   cin>>b;
+}
+*/
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    int *p,x;
+    p=x;
+}
+*/
+/*#include<iostream>
+using namespace std;
+int x;
+void setValue(int a)
+{
+    x=a;
+}
+int main()
+{
+    setValue(2);
+      setValue(3);
+      cout<<x;
+}
+*/
+/*#include<iostream>
+using namespace std;
+int x,y;
+int main()
+{
+
+    x=2,y=5;
+    3=x;
+    cout<<x<<endl;
+    int arr[4]={10,25,30,40};
+    for(int i=0;i<4;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            cout<<arr[i]<<" ";
+        }
+    }
+}
+*/
+
+/*#include<iostream>
+using namespace std;
+int a,b;
+int main(void)
+{
+    int x,a;
+    x=x+2;
+    a=4;int x=9;
+}*/
+
+
